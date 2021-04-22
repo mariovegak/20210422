9d8dc54ab285cef77b9b480853d9bb89d29d20bb
Mario Vega Kolkatin <61027560+mariovegak@users.noreply.github.com>
2021-04-22 10:55:40 -0400

    Add files via upload

--- ConectarPythonAPITwitter.py
+++ ConectarPythonAPITwitter.py
@@ -1,22 +1,27 @@
-# Biblioteca Interface Twitter
+# Import del Package Twitter
 import tweepy
 # Datos para Authentication
-consumer_key = 'consumer key'
-consumer_secret = 'consumer secret'
-accessToken = 'access Token'
-accessTokenSecret = 'access TokenSecret'
-# Ingresa al Api
+consumer_key = 'AGREGAR consumer_key'
+consumer_secret = 'AGREGAR consumer_secret'
+accessToken = 'AGREGAR accessToken'
+accessTokenSecret = 'AGREGAR accessTokenSecret'
+# Ingreso al Api
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(accessToken, accessTokenSecret)
 api = tweepy.API(auth)
 # Datos para busqueda
 keyword ='temblor'
-noOfTweet = 10
+noOfTweet = 50
+# Abro cursor
 tweets = tweepy.Cursor(api.search, q=keyword).items(noOfTweet)
 #imprime resultado de busqueda
 print("--------------")
 for tweet in tweets:
-    print(tweet.text)
-    print("--------------")
+    if "Chile" in tweet.user.location:
+        print(tweet.user.screen_name)
+        print(tweet.user.location)
+        print(tweet.created_at)
+        print(tweet.text)
+        print("--------------")
 
 
